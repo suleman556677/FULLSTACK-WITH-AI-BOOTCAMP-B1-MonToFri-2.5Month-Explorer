7d5861b4da684b59fdff22407a40581a506e478f
suleman556677 <muhammadsuleman302.307@gmail.com>
2025-07-10 21:27:57 +0500

    es

--- Week1/Case3-HelloMe.py
+++
@@ -1,9 +0,0 @@
-# First program
-print ("Starts here")
-
-name = input("Please enter your name: ")
-
-print("Hello - welocme - " + name)
-
-print("ends here")
-
--- Week1/Case4-Variable.py
+++
@@ -1,7 +0,0 @@
-Age = 34
-Name="Richard"
-
-print ("Age: " + str(Age))
-print("Name: " + Name)
-
-print("ends here")
