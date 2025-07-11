2421695c9a9927c91d2f470dc2c7c52352fe28e0
ShahzadSarwar10 <e@cognitiveconvergence.com>
2025-07-11 15:38:05 +0500

    Minor path Fix

--- Week4/Case4-5-LogisticRegressionViaSciKitLearn.py
+++ Week4/Case4-5-LogisticRegressionViaSciKitLearn.py
@@ -9,7 +9,7 @@ https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticR
 import pandas as pd
 col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
 # load dataset
-pima = pd.read_csv("week5/diabetes.csv", header=1, names=col_names)
+pima = pd.read_csv("week4/diabetes.csv", header=1, names=col_names)
 
 """
 
--- Week4/Case4-6-DecisionTreeClassificationViaSciKitLearn.py
+++ Week4/Case4-6-DecisionTreeClassificationViaSciKitLearn.py
@@ -11,7 +11,7 @@ from sklearn import metrics #Import scikit-learn metrics module for accuracy cal
 
 col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
 # load dataset
-pima = pd.read_csv("week5/diabetes.csv", header=1, names=col_names)
+pima = pd.read_csv("week4/diabetes.csv", header=1, names=col_names)
 
 
 """
